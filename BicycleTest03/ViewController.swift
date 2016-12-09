1c2b8bda57921dd4aa92ae177428622e70ee1e39
Lee gun joon <kirkee2@naver.com>
2016-12-09 18:06:55 +0900

    Initial Commit

---
+++ BicycleTest03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BicycleTest03
+//
+//  Created by 이 건준 on 2016. 12. 9..
+//  Copyright © 2016년 SSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
